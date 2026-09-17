1c3c322591f801936d4c01c7ea86826049f5fed6
SamDelorean <184150317+SamDelorean@users.noreply.github.com>
2026-09-17 12:55:01 -0600

    Model DMG and SGB stock clock drift separately

--- emulator/src/clock_mode.h
+++ emulator/src/clock_mode.h
@@ -1,6 +1,8 @@
 #ifndef GBCRT_CLOCK_MODE_H
 #define GBCRT_CLOCK_MODE_H
 
+#include "source_model.h"
+
 #include <stdbool.h>
 
 typedef enum {
@@ -10,28 +12,40 @@ typedef enum {
 
 typedef struct {
     gbcrt_clock_mode_t mode;
+    gbcrt_source_model_t source_model;
     double source_phase;
     unsigned long long output_frames;
     unsigned long long source_frames;
     unsigned long long repeated_output_frames;
+    unsigned long long skipped_source_frames;
 } gbcrt_clock_scheduler_t;
 
 const char *gbcrt_clock_mode_name(gbcrt_clock_mode_t mode);
-double gbcrt_gb_clock_hz(gbcrt_clock_mode_t mode);
-double gbcrt_gb_frame_hz(gbcrt_clock_mode_t mode);
+double gbcrt_gb_clock_hz(gbcrt_clock_mode_t mode,
+                         gbcrt_source_model_t source_model);
+double gbcrt_gb_frame_hz(gbcrt_clock_mode_t mode,
+                         gbcrt_source_model_t source_model);
 double gbcrt_ppu_frame_hz(void);
-double gbcrt_source_frames_per_ppu_frame(gbcrt_clock_mode_t mode);
-double gbcrt_stock_frame_slip_seconds(void);
+double gbcrt_source_frames_per_ppu_frame(gbcrt_clock_mode_t mode,
+                                         gbcrt_source_model_t source_model);
+double gbcrt_stock_frame_slip_seconds(gbcrt_source_model_t source_model);
 
 void gbcrt_clock_scheduler_init(gbcrt_clock_scheduler_t *scheduler,
-                                gbcrt_clock_mode_t mode);
+                                gbcrt_clock_mode_t mode,
+                                gbcrt_source_model_t source_model);
 void gbcrt_clock_scheduler_set_mode(gbcrt_clock_scheduler_t *scheduler,
                                     gbcrt_clock_mode_t mode);
+
 /*
- * Call once per RP2C02 output frame. Returns true when a new Game Boy source
- * frame should be generated. In stock-clock mode this naturally repeats an
- * occasional source frame; in synchronized mode it advances 1:1.
+ * Call once per RP2C02 output frame. The return value is the number of source
+ * frames that should be consumed before presenting this output frame:
+ *
+ *   0 -> repeat the previous Game Boy frame (stock DMG drift)
+ *   1 -> normal one-to-one advance
+ *   2 -> consume two source frames and present the newest one (stock SGB drift)
+ *
+ * Synchronized mode always returns 1.
  */
-bool gbcrt_clock_scheduler_step(gbcrt_clock_scheduler_t *scheduler);
+unsigned gbcrt_clock_scheduler_step(gbcrt_clock_scheduler_t *scheduler);
 
 #endif
